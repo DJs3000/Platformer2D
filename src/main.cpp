b2f4805341bafd80214ae8918346a7d8bea711b9
Albrict <albrict.work@gmail.com>
2025-03-09 15:24:01 +0200

    Switched to raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,12 @@
-#include "game.hpp"
-#include "error.hpp"
-#include <cstdlib>
-#include <iostream>
+#include <raylib.h>
 
-int main(int argc, char **argv)
+int main()
 {
-    try {
-        const bool is_initialized = Platformer2D::Init();
-        if (is_initialized == false) {
-            std::cerr << "Can't launch Tilengine..." << std::endl;
-            return EXIT_FAILURE;
-        } 
-        Platformer2D::Run();
-        Platformer2D::Close();
-        return EXIT_SUCCESS;
-    } catch(const std::exception &ex) {
-        Error::LogError(ex.what()); 
-        Platformer2D::Close();
-        return EXIT_FAILURE;
-    } catch(...) {
-        Error::LogError("Unknown error!");
-        Platformer2D::Close();
-        return EXIT_FAILURE;
+    InitWindow(800, 600, "Platformer");
+    while(!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(BLACK);
+        EndDrawing();
     }
+    CloseWindow();
 }
--- src/utils.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Tilengine.h>
-#include "error.hpp"
-
-namespace Utils {
-}
